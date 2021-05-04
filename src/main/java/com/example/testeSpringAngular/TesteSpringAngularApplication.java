2ad9c432a0573cb290d71d08fa9dc04bd8cb3584
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-05-04 17:24:23 -0300

    Projeto Iniciado - Testando Spring e Angular

---
+++ src/main/java/com/example/testeSpringAngular/TesteSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.testeSpringAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringAngularApplication.class, args);
+	}
+
+}
