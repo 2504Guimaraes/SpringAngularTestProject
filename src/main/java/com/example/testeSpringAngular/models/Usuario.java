c29dd7accaa93bf6d0e7750db481f76c0a4d2544
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-05-05 02:00:52 -0300

    Consertando erro na classe Usuário
    adicionando getters, setters e sobrescrita do método toString que eu havia esquecido :P

--- src/main/java/com/example/testeSpringAngular/models/Usuario.java
+++ src/main/java/com/example/testeSpringAngular/models/Usuario.java
@@ -20,4 +20,34 @@ public class Usuario {
         this.nome = nome;
         this.email = email;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "{ nome: " + this.nome + ", email: " +
+                this.email + " }";
+    }
 }
