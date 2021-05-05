349b170d3a8e9065f29e9087563fcfe4c4f6be6e
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-05-05 02:44:09 -0300

    criando serviço ng p/ usuário no front

--- src/main/java/com/example/testeSpringAngular/models/Usuario.java
+++ src/main/java/com/example/testeSpringAngular/models/Usuario.java
@@ -14,7 +14,7 @@ public class Usuario {
     private String nome;
     private String email;
 
-    public Usuario() {}
+    public Usuario() { }
 
     public Usuario(String nome, String email) {
         this.nome = nome;
--- src/main/resources/templates/frontend/src/app/services/user-service.service.ts
+++ src/main/resources/templates/frontend/src/app/services/user-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Usuario } from "../models/usuario";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  private urlUsuario: string = "";
+
+  constructor(private http: HttpClient) {
+    this.urlUsuario = "http://localhost:8080/usuarios";
+  }
 }
