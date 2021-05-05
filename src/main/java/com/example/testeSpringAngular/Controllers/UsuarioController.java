919e57e4b01ce22030c46b9ccbd23013f929a173
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-05-05 02:41:38 -0300

    adicionando identificador @Autowired p/ this.usuarioRepositorio não começar indefinido e dar erro :P

--- src/main/java/com/example/testeSpringAngular/Controllers/UsuarioController.java
+++ src/main/java/com/example/testeSpringAngular/Controllers/UsuarioController.java
@@ -2,6 +2,7 @@ package com.example.testeSpringAngular.Controllers;
 
 import com.example.testeSpringAngular.Repositories.UsuarioRepositorio;
 import com.example.testeSpringAngular.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,11 +10,13 @@ import java.util.List;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
+
+    @Autowired
     private UsuarioRepositorio usuarioRepositorio;
 
     @GetMapping("/usuarios")
     public List<Usuario> getUsers() {
-        return (List<Usuario>)  usuarioRepositorio.findAll();
+        return (List<Usuario>) usuarioRepositorio.findAll();
     }
 
     @PostMapping("/usuarios")
