f31ff2e24493e57563bc69324b6f9f4a6e116d23
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-05-05 03:02:23 -0300

    configurando ng user-service p/ consumir API Rest no back-end

--- src/main/resources/templates/frontend/src/app/services/user-service.service.ts
+++ src/main/resources/templates/frontend/src/app/services/user-service.service.ts
@@ -13,4 +13,13 @@ export class UserServiceService {
   constructor(private http: HttpClient) {
     this.urlUsuario = "http://localhost:8080/usuarios";
   }
+
+  public findAll(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.urlUsuario);
+  }
+
+  public save(usuario: Usuario) {
+    return this.http.post<Usuario>(this.urlUsuario, usuario);
+  }
+
 }
